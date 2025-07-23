da35f41580d91aab13bb70a6868debd0241186d9
bhoomigundecha <bhoomigundecha@gmail.com>
2025-07-23 21:53:23 +0530

    Further Routes Updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const Listing = require("./models/listing.js");
 const path = require("path");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderer';
-
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 
 main().then(()=>{
@@ -19,6 +19,7 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 app.get("/", (req, res)=>{
     res.send("Hello I am root");
 });
@@ -52,10 +53,25 @@ app.get("/listings/:id", async(req, res)=>{
     res.render("listings/show.ejs", {listing});
 });
 
-app.get("/listings/:id/edit", (req, res)=>{
-    
-})
+app.get("/listings/:id/edit", async (req, res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
 
+app.put("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    console.log(req.body);
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+// Delete the route 
+
+app.delete("/listings/:id", async(req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
 
 app.listen(8080, ()=>{
     console.log(`Listening to port 8080`);
