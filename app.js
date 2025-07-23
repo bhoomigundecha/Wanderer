b4de1346334240eaeb9cd5e06685f4ff6b94f32b
bhoomigundecha <bhoomigundecha@gmail.com>
2025-07-23 22:11:32 +0530

    BoilerPlate code added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderer';
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
-
+const ejsMate = require("ejs-mate");
 main().then(()=>{
     console.log("Connected to DB");
 })
@@ -20,6 +20,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+
 app.get("/", (req, res)=>{
     res.send("Hello I am root");
 });
